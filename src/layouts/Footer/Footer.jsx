8b239e1d4f6aa1157ae8c6c647671b2ec8932e8f
JoelHellberg95 <Joel@alnina.se>
2025-05-13 00:50:37 +0200

    Updated Footer.jsx

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -7,10 +7,10 @@ function Footer() {
             <img src={logo} alt="Hellberg Systems" className="footer-logo" />
 
             <div className="footer-links">
-                <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+                <a href="https://github.com/JoelHellberg95" target="_blank" rel="noopener noreferrer">
                     <i className="fa-brands fa-github"></i>
                 </a>
-                <a href="https://spotify.com" target="_blank" rel="noopener noreferrer">
+                <a href="https://open.spotify.com/playlist/679wCT6dVMDBxrYa5NcrXL?si=4b87aa257b4e4a3a" target="_blank" rel="noopener noreferrer">
                     <i className="fa-brands fa-spotify"></i>
                 </a>
                 <a href="https://discord.gg/GJe563jP" target="_blank" rel="noopener noreferrer">
