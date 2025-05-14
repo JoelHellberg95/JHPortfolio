3edceb3e0cdf62c7ef2dfd143ecc236178124033
JoelHellberg95 <Joel@alnina.se>
2025-05-14 21:08:57 +0200

    Added Vercel Speedinsights

--- src/features/About/About.jsx
+++ src/features/About/About.jsx
@@ -3,17 +3,22 @@ import TechStack from '../TechStack/TechStack';
 function About() {
     return (
         <section className="about-section">
-            <h2>Om mig</h2>
-            <img className={"profile-pic"} src="/ai-joel.jpg" alt="Joel" />
             <div className="about-content">
                 <div className="about-text">
-                    <p>Hej! Jag heter Joel och är en mångsidig utvecklare med fokus på frontend, backend och praktisk problemlösning. Jag gillar att bygga smarta system, snygga gränssnitt och robusta API:er.</p>
+                    <h2>Om mig</h2>
+                    <p>Hej! Jag heter Joel och är en mångsidig utvecklare med fokus på frontend, backend och praktisk problemlösning.
+                        Jag gillar att bygga smarta system, snygga gränssnitt och robusta API:er.</p>
                     <p>Med erfarenhet från både IT-support, grafisk design och webbutveckling har jag en bred förståelse för vad som krävs – både tekniskt och användarmässigt.</p>
                     <p>När jag inte kodar? Då hittar du mig ute på gården, pysslandes med projekt eller bland maskiner.</p>
+                </div>
+                <div className="image-container">
+                    <img src="/ai-joel-transparent.png" alt="Porträttbild på Joel" className="profile-pic" />
+
                 </div>
             </div>
             <TechStack />
         </section>
+
 
     );
 }
